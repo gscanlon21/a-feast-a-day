7c899e7c41884fbd822bdf5ba51c56de24d15d1e
gscanlon <git@ascallion.com>
2024-05-29 18:28:30 -0500

    improvement; nutrient order

--- Core/Models/User/Nutrients.cs
+++ Core/Models/User/Nutrients.cs
@@ -66,31 +66,31 @@ public enum Nutrients : long
     Cholesterol = HDLCholesterol | LDLCholesterol, // 3072
 
     // Vitamins
-    [Display(Name = "Vitamin A (Retinoids)", GroupName = "Vitamin A", Description = "beef, liver, eggs, shrimp, fish, fortified milk, sweet potatoes, carrots, pumpkins, spinach, mangoes")]
+    [Display(Name = "Vitamin A (Retinoids)", GroupName = "Vitamins")]
     VitaminARetinoids = 1 << 15, // 32768
 
-    [Display(Name = "Vitamin A (Carotenoids)", GroupName = "Vitamin A", Description = "beef, liver, eggs, shrimp, fish, fortified milk, sweet potatoes, carrots, pumpkins, spinach, mangoes")]
+    [Display(Name = "Vitamin A (Carotenoids)", GroupName = "Vitamins")]
     VitaminACartenoids = 1 << 16, // 65536
 
-    [Display(ShortName = "Vitamin A", Name = "Vitamin A (Retinoids and Carotenoids)", GroupName = "Vitamin A", Description = "beef, liver, eggs, shrimp, fish, fortified milk, sweet potatoes, carrots, pumpkins, spinach, mangoes")]
+    [Display(ShortName = "Vitamin A", Name = "Vitamin A (Retinoids and Carotenoids)", GroupName = "Vitamins")]
     VitaminA = VitaminARetinoids | VitaminACartenoids, // 98304
 
-    [Display(ShortName = "Vitamin B1", Name = "Vitamin B1 (Thiamine)", GroupName = "Vitamin B", Description = "ham, soymilk, watermelon, acorn squash")]
+    [Display(ShortName = "Vitamin B1", Name = "Vitamin B1 (Thiamine)", GroupName = "Vitamins")]
     VitaminB1 = 1 << 17, // 131072
 
-    [Display(ShortName = "Vitamin B2", Name = "Vitamin B2 (Riboflavin)", GroupName = "Vitamin B", Description = "milk, yogurt, cheese, whole and enriched grains and cereals.")]
+    [Display(ShortName = "Vitamin B2", Name = "Vitamin B2 (Riboflavin)", GroupName = "Vitamins")]
     VitaminB2 = 1 << 18, // 262144
 
-    [Display(ShortName = "Vitamin B3", Name = "Vitamin B3 (Niacin)", GroupName = "Vitamin B", Description = "meat, poultry, fish, fortified and whole grains, mushrooms, potatoes")]
+    [Display(ShortName = "Vitamin B3", Name = "Vitamin B3 (Niacin)", GroupName = "Vitamins")]
     VitaminB3 = 1 << 19, // 524288
 
-    [Display(ShortName = "Vitamin B5", Name = "Vitamin B5 (Pantothenic Acid)", GroupName = "Vitamin B", Description = "chicken, whole grains, broccoli, avocados, mushrooms")]
+    [Display(ShortName = "Vitamin B5", Name = "Vitamin B5 (Pantothenic Acid)", GroupName = "Vitamins")]
     VitaminB5 = 1 << 20, // 1048576
 
-    [Display(ShortName = "Vitamin B6", Name = "Vitamin B6 (Pyridoxine)", GroupName = "Vitamin B", Description = "meat, fish, poultry, legumes, tofu and other soy products, bananas")]
+    [Display(ShortName = "Vitamin B6", Name = "Vitamin B6 (Pyridoxine)", GroupName = "Vitamins")]
     VitaminB6 = 1 << 21, // 2097152
 
-    [Display(ShortName = "Vitamin B7", Name = "Vitamin B7 (Biotin)", GroupName = "Vitamin B", Description = "Whole grains, eggs, soybeans, fish")]
+    [Display(ShortName = "Vitamin B7", Name = "Vitamin B7 (Biotin)", GroupName = "Vitamins")]
     VitaminB7 = 1 << 22, // 4194304
 
     /// <summary>
@@ -99,85 +99,85 @@ public enum Nutrients : long
     /// to folate deficiency and other adverse side effects!
     /// https://education.seekinghealth.com/folic-acid-side-effects/
     /// </summary>
-    [Display(ShortName = "Vitamin B9", Name = "Vitamin B9 (Folinic Acid)", GroupName = "Vitamin B", Description = "Fortified grains and cereals, asparagus, spinach, broccoli, legumes (black-eyed peas and chickpeas), orange juice")]
+    [Display(ShortName = "Vitamin B9", Name = "Vitamin B9 (Folinic Acid)", GroupName = "Vitamins")]
     VitaminB9 = 1 << 23, // 8388608
 
-    [Display(ShortName = "Vitamin B12", Name = "Vitamin B12 (Cobalamin)", GroupName = "Vitamin B", Description = "Meat, poultry, fish, milk, cheese, fortified soymilk and cereals")]
+    [Display(ShortName = "Vitamin B12", Name = "Vitamin B12 (Cobalamin)", GroupName = "Vitamins")]
     VitaminB12 = 1 << 24, // 16777216
 
-    [Display(ShortName = "Vitamin C", Name = "Vitamin C (Ascorbic Acid)", GroupName = "Vitamin C", Description = "Citrus fruit, potatoes, broccoli, bell peppers, spinach, strawberries, tomatoes, Brussels sprouts")]
+    [Display(ShortName = "Vitamin C", Name = "Vitamin C (Ascorbic Acid)", GroupName = "Vitamins")]
     VitaminC = 1 << 25, // 33554432
 
-    [Display(ShortName = "Vitamin D", Name = "Vitamin D (Calciferol)", GroupName = "Vitamin D", Description = "Fortified milk and cereals, fatty fish")]
+    [Display(ShortName = "Vitamin D", Name = "Vitamin D (Calciferol)", GroupName = "Vitamins")]
     VitaminD = 1 << 26, // 67108864
 
-    [Display(ShortName = "Vitamin E", Name = "Vitamin E (Alpha-Tocopherol)", GroupName = "Vitamin E", Description = "vegetables oils, leafy green vegetables, whole grains, nuts")]
+    [Display(ShortName = "Vitamin E", Name = "Vitamin E (Alpha-Tocopherol)", GroupName = "Vitamins")]
     VitaminE = 1 << 27, // 134217728
 
-    [Display(ShortName = "Vitamin K", Name = "Vitamin K (Phylloquinone, Menadione)", GroupName = "Vitamin K", Description = "Cabbage, eggs, milk, spinach, broccoli, kale")]
+    [Display(ShortName = "Vitamin K", Name = "Vitamin K (Phylloquinone, Menadione)", GroupName = "Vitamins")]
     VitaminK = 1 << 28, // 268435456,
 
     // Major Minerals
-    [Display(Name = "Calcium", GroupName = "Fruits and Vegatables", Description = "yogurt, cheese, milk, salmon, leafy green vegetables")]
+    [Display(Name = "Calcium", GroupName = "Minerals")]
     Calcium = 1L << 30, // 1073741824
 
-    [Display(Name = "Chloride", GroupName = "Fruits and Vegetables", Description = "salt")]
+    [Display(Name = "Chloride", GroupName = "Minerals")]
     Chloride = 1L << 31, // 2147483648
 
-    [Display(Name = "Magnesium", GroupName = "Unsaturated Fats and Cholesterol", Description = "Spinach, broccoli, legumes, seeds, whole-wheat bread")]
+    [Display(Name = "Magnesium", GroupName = "Minerals")]
     Magnesium = 1L << 32, // 4294967296
 
-    [Display(Name = "Potassium", GroupName = "Unsaturated Fats and Cholesterol", Description = "meat, milk, fruits, vegetables, grains, legumes")]
+    [Display(Name = "Potassium", GroupName = "Minerals")]
     Potassium = 1L << 33, // 8589934592
 
-    [Display(Name = "Sodium", GroupName = "Dairy", Description = "salt, soy sauce, vegetables")]
+    [Display(Name = "Sodium", GroupName = "Minerals")]
     Sodium = 1L << 34, // 17179869184
 
     // Trace Minerals
-    [Display(Name = "Chromium", GroupName = "Whole Grains", Description = "meat, poultry, fish, nuts, cheese")]
+    [Display(Name = "Chromium", GroupName = "Minerals")]
     Chromium = 1L << 35, // 34359738368
 
-    [Display(Name = "Copper", GroupName = "Fish, Poultry, and Eggs", Description = "shellfish, nuts, seeds, whole-grain products, beans, prunes")]
+    [Display(Name = "Copper", GroupName = "Minerals")]
     Copper = 1L << 36, // 68719476736
 
-    [Display(Name = "Fluoride", GroupName = "Fish, Poultry, and Eggs", Description = "fish, teas")]
+    [Display(Name = "Fluoride", GroupName = "Minerals")]
     Fluoride = 1L << 37, // 137438953472
 
-    [Display(Name = "Iodine", GroupName = "Fish, Poultry, and Eggs", Description = "Iodized salt, seafood")]
+    [Display(Name = "Iodine", GroupName = "Minerals")]
     Iodine = 1L << 38, // 274877906944
 
-    [Display(Name = "Iron", GroupName = "Nuts, Seeds, Beans, and Tofu", Description = "red meat, poultry, eggs, fruits, green vegetables, fortified bread")]
+    [Display(Name = "Iron", GroupName = "Minerals")]
     Iron = 1L << 39, // 549755813888
 
-    [Display(Name = "Manganese", GroupName = "Nuts, Seeds, Beans, and Tofu", Description = "nuts, legumes, whole grains, tea")]
+    [Display(Name = "Manganese", GroupName = "Minerals")]
     Manganese = 1L << 40, // 1099511627776
 
-    [Display(Name = "Selenium", GroupName = "Nuts, Seeds, Beans, and Tofu", Description = "Organ meat, seafood, walnuts")]
+    [Display(Name = "Selenium", GroupName = "Minerals")]
     Selenium = 1L << 41, // 2199023255552
 
-    [Display(Name = "Zinc", GroupName = "Nuts, Seeds, Beans, and Tofu", Description = "meat, shellfish, legumes, whole grains")]
+    [Display(Name = "Zinc", GroupName = "Minerals")]
     Zinc = 1L << 42, // 4398046511104,
 
-    [Display(Name = "Molybdenum", GroupName = "", Description = "")]
+    [Display(Name = "Molybdenum", GroupName = "Minerals")]
     Molybdenum = 1L << 43, // 8796093022208,
 
-    [Display(Name = "Phosphorus", GroupName = "", Description = "")]
+    [Display(Name = "Phosphorus", GroupName = "Minerals")]
     Phosphorus = 1L << 44, // 17592186044416,
 
-    [Display(Name = "Sulfur", GroupName = "", Description = "")]
+    [Display(Name = "Sulfur", GroupName = "Minerals")]
     Sulfur = 1L << 45, // 35184372088832,
 
-    [Display(Name = "Boron", GroupName = "", Description = "")]
+    [Display(Name = "Boron", GroupName = "Minerals")]
     Boron = 1L << 46, // 70368744177664,
 
-    [Display(Name = "Vanadium", GroupName = "", Description = "")]
+    [Display(Name = "Vanadium", GroupName = "Minerals")]
     Vanadium = 1L << 47, // 140737488355328,
 
     // Other Essential Nutrients
-    [Display(Name = "Lithium", GroupName = "Lithium", Description = "Lithium")]
+    [Display(Name = "Lithium", GroupName = "Micronutrients")]
     Lithium = 1L << 50, // 1125899906842624,
 
-    [Display(Name = "Choline", GroupName = "Choline", Description = "Choline")]
+    [Display(Name = "Choline", GroupName = "Micronutrients")]
     Choline = 1L << 51, // 2251799813685248,
 
     All = Proteins | Starch | SolubleFiber | InsolubleFiber | Sugar | Oligosaccharides
