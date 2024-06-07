753a2c1567f26fb80d04986608690afd2270ae8b
gscanlon <git@ascallion.com>
2024-06-07 12:14:46 -0500

    fix: spacing

--- Web/ViewModels/User/UserFamilyViewModel.cs
+++ Web/ViewModels/User/UserFamilyViewModel.cs
@@ -9,11 +9,14 @@ public class UserFamilyViewModel
     public int UserId { get; set; }
 
     [Range(UserFamily.Consts.WeightMin, UserFamily.Consts.WeightMax)]
+    [Display(Name = "Weight (kg)")]
     public int Weight { get; init; } = UserFamily.Consts.WeightDefault;
 
     [Range(UserFamily.Consts.CaloriesPerDayMin, UserFamily.Consts.CaloriesPerDayMax)]
+    [Display(Name = "Calories Per Day")]
     public int CaloriesPerDay { get; init; } = UserFamily.Consts.CaloriesPerDayDefault;
 
+    [Display(Name = "Person")]
     public Person Person { get; init; }
 
     public bool Hide { get; set; }
