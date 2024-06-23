d1025eb5cba6737bcfd9b74d0406b0661f648aed
gscanlon <git@ascallion.com>
2024-06-23 18:22:56 -0500

    feat: equipment

--- Core/Models/Recipe/Equipment.cs
+++ Core/Models/Recipe/Equipment.cs
@@ -7,6 +7,27 @@ public enum Equipment
 {
     None = 0,
 
-    [Display(Name = "Crockpot")]
-    Crockpot = 1 << 0, // 1
+    [Display(Name = "Microwave")]
+    Microwave = 1 << 0, // 1
+
+    [Display(Name = "Oven")]
+    Oven = 1 << 1, // 2
+
+    [Display(Name = "Stove")]
+    Stove = 1 << 2, // 4
+
+    [Display(Name = "Toaster")]
+    Toaster = 1 << 3, // 8
+
+    [Display(Name = "Blender")]
+    Blender = 1 << 4, // 16
+
+    [Display(Name = "Slow Cooker")]
+    SlowCooker = 1 << 5, // 32
+
+    [Display(Name = "Toaster Oven")]
+    ToasterOven = 1 << 6, // 64
+
+    [Display(Name = "Bread Maker")]
+    BreadMaker = 1 << 7, // 128
 }
