a4d305a428f8eb0308a170e731903f417d0e11b9
gscanlon <git@ascallion.com>
2024-07-10 12:15:37 -0500

    fix: order

--- Data/Query/QueryRunner.cs
+++ Data/Query/QueryRunner.cs
@@ -198,10 +198,12 @@ public class QueryRunner(Section section)
         var allFilteredResultIngredientIds = filteredResults.SelectMany(qr => qr.RecipeIngredients.Select(ri => ri.Ingredient?.Id)).ToList();
         var allNutrients = await context.Nutrients.Where(n => allFilteredResultIngredientIds.Contains(n.IngredientId)).ToListAsync();
         foreach (var recipe in filteredResults
-            // Show exercise variations that the user has rarely seen.
+            // Order by recipes that are still pending refresh.
+            .OrderByDescending(a => a.UserRecipe?.RefreshAfter.HasValue)
+            // Show recipes that the user has rarely seen.
             // Adding the two in case there is a warmup and main variation in the same exercise.
             // ... Otherwise, since the warmup section is always chosen first, the last seen date is always updated and the main variation is rarely chosen.
-            .OrderBy(a => a.UserRecipe?.LastSeen.DayNumber)
+            .ThenBy(a => a.UserRecipe?.LastSeen.DayNumber)
             // Mostly for the demo, show mostly random exercises.
             // NOTE: When the two variation's LastSeen dates are the same:
             // ... The LagRefreshXWeeks will prevent the LastSeen date from updating
