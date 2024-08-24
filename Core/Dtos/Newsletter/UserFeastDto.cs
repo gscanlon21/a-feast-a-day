510d51de6d5d7fe62b182ac7774e456153dc9c8d
gscanlon <git@ascallion.com>
2024-08-24 09:03:40 -0500

    fix: list title and desc

--- Core/Dtos/Newsletter/UserFeastDto.cs
+++ Core/Dtos/Newsletter/UserFeastDto.cs
@@ -16,4 +16,14 @@ public class UserFeastDto
     public DateOnly Date { get; init; }
 
     public string? Logs { get; init; }
+
+    /// <summary>
+    /// Title to display for the list item in the app.
+    /// </summary>
+    public string Title() => Date.ToLongDateString();
+
+    /// <summary>
+    /// Description to display for the list item in the app.
+    /// </summary>
+    public string Description() => $"{Date}";
 }
