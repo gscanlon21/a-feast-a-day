8b3288dc018f8db2fe0a14f21eaa9b8d1d8dfc5f
gscanlon <git@ascallion.com>
2024-10-27 18:39:30 -0500

    chore: update database backup

--- Core/Models/Ingredients/Category.cs
+++ Core/Models/Ingredients/Category.cs
@@ -22,90 +22,105 @@ public enum Category
     [Display(Name = "Condiments", Order = 5)]
     Condiments = 1 << 4, // 16
 
-    [Display(Name = "Canned Goods", Order = 6)]
-    CannedGoods = 1 << 5, // 32
+    [Display(Name = "Bread", Order = 6)]
+    Bread = 1 << 5, // 32
 
-    [Display(Name = "Container Goods", Order = 7)]
-    ContainerGoods = Condiments | CannedGoods, // 48
+    [Display(Name = "Canned Goods", Order = 7)]
+    CannedGoods = 1 << 6, // 64
 
-    [Display(Name = "Grains", Order = 8)]
-    Grains = 1 << 6, // 64
+    [Display(Name = "Jarred Goods", Order = 8)]
+    JarredGoods = 1 << 7, // 128
 
-    [Display(Name = "Legumes", Order = 9)]
-    Legumes = 1 << 7, // 128
+    [Display(Name = "Container Goods", Order = 9)]
+    ContainerGoods = Condiments | CannedGoods | JarredGoods,
 
-    [Display(Name = "Boxed Goods", Order = 10)]
-    BoxedGoods = Grains | Legumes, // 192
+    [Display(Name = "Grains", Order = 10)]
+    Grains = 1 << 8, // 256
 
-    [Display(Name = "Dry Goods", Order = 11)]
-    DryGoods = ContainerGoods | BoxedGoods, // 240
+    [Display(Name = "Legumes", Order = 11)]
+    Legumes = 1 << 9, // 512
 
-    [Display(Name = "Chocolate", Order = 12)]
-    Chocolate = 1 << 8, // 256
+    [Display(Name = "Noodles", Order = 12)]
+    Noodles = 1 << 10, // 1024
 
-    [Display(Name = "Flour", Order = 13)]
-    Flour = 1 << 9, // 512
+    [Display(Name = "Boxed Goods", Order = 13)]
+    BoxedGoods = Grains | Legumes | Noodles,
 
-    [Display(Name = "Oils", Order = 14)]
-    Oils = 1 << 10, // 1024
+    [Display(Name = "Rice", Order = 14)]
+    Rice = 1 << 11, // 2048
 
-    [Display(Name = "Baking Goods", Order = 15)]
-    BakingGoods = Chocolate | Flour | Oils, // 1792,
+    [Display(Name = "Foreign", Order = 15)]
+    Foreign = 1 << 12, // 4092
 
-    [Display(Name = "Spices", Order = 16)]
-    Spices = 1 << 11, // 2048
+    [Display(Name = "Dry Goods", Order = 16)]
+    DryGoods = ContainerGoods | BoxedGoods | Rice | Foreign,
 
-    [Display(Name = "Crackers", Order = 17)]
-    Crackers = 1 << 12, // 4092
+    [Display(Name = "Chocolate", Order = 17)]
+    Chocolate = 1 << 13, // 8192,
 
-    [Display(Name = "Beverages", Order = 18)]
-    Beverages = 1 << 13, // 8192,
+    [Display(Name = "Flour", Order = 18)]
+    Flour = 1 << 14, // 16384,
 
-    [Display(Name = "Nuts", Order = 19)]
-    Nuts = 1 << 14, // 16384,
+    [Display(Name = "Oils", Order = 19)]
+    Oils = 1 << 15, // 32768,
 
-    [Display(Name = "Snacks", Order = 20)]
-    Chips = 1 << 15, // 32768,
+    [Display(Name = "Baking Goods", Order = 20)]
+    BakingGoods = Chocolate | Flour | Oils,
 
-    [Display(Name = "Snacks", Order = 21)]
-    Snacks = Nuts | Chips, // 49152
+    [Display(Name = "Spices", Order = 21)]
+    Spices = 1 << 16, // 65536
 
-    [Display(Name = "Milk", Order = 22)]
-    Milk = 1 << 16, // 65536
+    [Display(Name = "Crackers", Order = 22)]
+    Crackers = 1 << 17, // 131072
 
-    [Display(Name = "Cheese", Order = 23)]
-    Cheese = 1 << 17, // 131072
+    [Display(Name = "Beverages", Order = 23)]
+    Beverages = 1 << 18, // 262144
 
-    [Display(Name = "Eggs", Order = 24)]
-    Eggs = 1 << 18, // 262144
+    [Display(Name = "Nuts", Order = 24)]
+    Nuts = 1 << 19, // 524288
 
-    [Display(Name = "Dairy", Order = 25)]
-    Dairy = Milk | Cheese | Eggs, // 458752,
+    [Display(Name = "Snacks", Order = 25)]
+    Chips = 1 << 20, // 1048576
 
-    [Display(Name = "Frozen Fruit", Order = 26)]
-    FrozenFruit = 1 << 19, // 524288
+    [Display(Name = "Snacks", Order = 26)]
+    Snacks = Nuts | Chips,
 
-    [Display(Name = "Frozen Vegetables", Order = 27)]
-    FrozenVegetables = 1 << 20, // 1048576
+    [Display(Name = "Milk", Order = 27)]
+    Milk = 1 << 21, // 2097152
 
-    [Display(Name = "Frozen Meat", Order = 28)]
-    FrozenMeat = 1 << 21, // 2097152
+    [Display(Name = "Cheese", Order = 28)]
+    Cheese = 1 << 22, // 4194304
 
-    [Display(Name = "Frozen Goods", Order = 29)]
-    FrozenGoods = FrozenFruit | FrozenVegetables | FrozenMeat, // 3670016,
+    [Display(Name = "Eggs", Order = 29)]
+    Eggs = 1 << 23, // 8388608
 
-    [Display(Name = "Meat", Order = 30)]
-    Meat = 1 << 22, // 4194304
+    [Display(Name = "Dairy", Order = 30)]
+    Dairy = Milk | Cheese | Eggs,
 
-    [Display(Name = "Fish", Order = 31)]
-    Fish = 1 << 23, // 8388608
+    [Display(Name = "Frozen Fruit", Order = 31)]
+    FrozenFruit = 1 << 24, // 16777216
 
-    [Display(Name = "Pet Care Items", Order = 32)]
-    PetCareItems = 1 << 24, // 16777216
+    [Display(Name = "Frozen Vegetables", Order = 32)]
+    FrozenVegetables = 1 << 25, // 33554432
 
-    [Display(Name = "Home Care Items", Order = 33)]
-    HomeCareItems = 1 << 25, // 33554432
+    [Display(Name = "Frozen Meat", Order = 33)]
+    FrozenMeat = 1 << 26, // 67108864
 
-    [Display(Name = "Personal Care Items", Order = 34)]
-    PersonalCareItems = 1 << 26, // 67108864
+    [Display(Name = "Frozen Goods", Order = 34)]
+    FrozenGoods = FrozenFruit | FrozenVegetables | FrozenMeat,
+
+    [Display(Name = "Meat", Order = 35)]
+    Meat = 1 << 27, // 134217728
+
+    [Display(Name = "Fish", Order = 36)]
+    Fish = 1 << 28, // 268435456
+
+    [Display(Name = "Pet Care Items", Order = 37)]
+    PetCareItems = 1 << 29, // 536870912
+
+    [Display(Name = "Home Care Items", Order = 38)]
+    HomeCareItems = 1 << 30, // 1073741824
+
+    [Display(Name = "Personal Care Items", Order = 39)]
+    PersonalCareItems = 1 << 31, // 2147483648
 }
