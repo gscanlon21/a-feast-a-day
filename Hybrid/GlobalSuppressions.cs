f3dab97a3e1df4a7120b7e642d2831b72088c990
gscanlon <git@ascallion.com>
2025-02-17 23:00:02 -0600

    fix: binding context

---
+++ Hybrid/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator", "MVVMTK0042:Prefer using [ObservableProperty] on partial properties", Justification = "Some have initilizers", Scope = "member", Target = "~F:Hybrid.ShoppingListPageViewModel._ingredients")]
