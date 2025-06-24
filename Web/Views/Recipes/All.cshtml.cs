b1b72efe33599f5c2769003afe9193a733e0a3db
Graham Scanlon <git@ascallion.com>
2025-06-24 13:05:29 -0500

    improvement: clear btn

--- Web/Views/Recipes/All.cshtml.cs
+++ Web/Views/Recipes/All.cshtml.cs
@@ -1,16 +1,15 @@
 ﻿using Core.Dtos.Newsletter;
 using Core.Models.Newsletter;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 
 namespace Web.Views.Recipes;
 
 public class RecipesViewModel
 {
-    public RecipesViewModel() { }
+    private readonly bool _formOpen;
 
     public IList<NewsletterRecipeDto> Recipes { get; set; } = null!;
-
-    public Verbosity Verbosity => Verbosity.Debug;
 
     [Display(Name = "Name")]
     public string? Name { get; init; }
@@ -21,6 +20,18 @@ public class RecipesViewModel
     [Display(Name = "Equipment")]
     public Equipment? Equipment { get; init; }
 
+    [ValidateNever]
+    public Verbosity Verbosity => Verbosity.Debug;
+
+    [ValidateNever]
+    public bool FormOpen
+    {
+        get => _formOpen || Recipes?.Any() != true;
+        // Needs to be settable for the 'Clear' btn.
+        init => _formOpen = value;
+    }
+
+    [ValidateNever]
     public bool FormHasData =>
         !string.IsNullOrWhiteSpace(Name)
         || Section.HasValue
