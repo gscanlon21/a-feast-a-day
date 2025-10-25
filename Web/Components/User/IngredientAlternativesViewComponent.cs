e79228dcd359b75a6222bd7f7bf0c085291e094a
Graham Scanlon <git@ascallion.com>
2025-10-24 19:18:25 -0500

    feat: alternative ingredient display

---
+++ Web/Components/User/IngredientAlternativesViewComponent.cs
@@ -0,0 +1,56 @@
+﻿using Core.Dtos.Ingredient;
+using Core.Dtos.User;
+using Data;
+using Data.Entities.Ingredient;
+using Data.Repos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Views.Shared.Components.IngredientAlternatives;
+
+namespace Web.Components.User;
+
+public class IngredientAlternativesViewComponent : ViewComponent
+{
+    /// <summary>
+    /// For routing.
+    /// </summary>
+    public const string Name = "IngredientAlternatives";
+
+    private readonly CoreContext _context;
+    private readonly UserRepo _userRepo;
+
+    public IngredientAlternativesViewComponent(CoreContext context, UserRepo userRepo)
+    {
+        _context = context;
+        _userRepo = userRepo;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(Data.Entities.User.User user, Ingredient ingredient)
+    {
+        var partialIngredients = await GetAlternativeIngredients(ingredient, user, partial: true);
+        var alternativeIngredients = await GetAlternativeIngredients(ingredient, user, partial: false);
+
+        var token = await _userRepo.AddUserToken(user, durationDays: 1);
+        return View("IngredientAlternatives", new IngredientAlternativesViewModel()
+        {
+            UserNewsletter = new UserNewsletterDto(user.AsType<UserDto>()!, token),
+            PartialIngredients = partialIngredients.ConvertAll(i => i.AsType<IngredientDto>()!),
+            AlternativeIngredients = alternativeIngredients.ConvertAll(i => i.AsType<IngredientDto>()!),
+        });
+    }
+
+    private async Task<List<Ingredient>> GetAlternativeIngredients(Ingredient ingredient, Data.Entities.User.User user, bool partial)
+    {
+        return await _context.IngredientAlternatives.AsNoTracking()
+            .Include(i => i.AlternativeIngredient).ThenInclude(ai => ai.Nutrients)
+            .Include(i => i.AlternativeIngredient).ThenInclude(ai => ai.Alternatives).ThenInclude(a => a.AlternativeIngredient)
+            .Include(i => i.AlternativeIngredient).ThenInclude(ai => ai.AlternativeIngredients).ThenInclude(ai => ai.Ingredient)
+            // The user is an admin who is allowed to edit alternative ingredients. Or the user owns the ingredient.
+            .Where(i => i.AlternativeIngredient.UserId == user.Id || i.AlternativeIngredient.UserId == null)
+            .Where(ia => ia.IngredientId == ingredient.Id)
+            .Where(ia => ia.IsAggregateElement == partial)
+            .OrderBy(f => f.AlternativeIngredient.Name)
+            .Select(ia => ia.AlternativeIngredient)
+            .ToListAsync();
+    }
+}
---
+++ Web/Views/Shared/Components/IngredientAlternatives/IngredientAlternatives.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Core.Dtos.Ingredient;
+using Core.Dtos.User;
+using Core.Models.Newsletter;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Views.Shared.Components.IngredientAlternatives;
+
+public class IngredientAlternativesViewModel
+{
+    [Display(Name = "Alternative Ingredients")]
+    public required IList<IngredientDto> AlternativeIngredients { get; init; }
+
+    [Display(Name = "Partial Ingredients")]
+    public required IList<IngredientDto> PartialIngredients { get; init; }
+
+    public required UserNewsletterDto UserNewsletter { get; init; }
+
+    public Verbosity Verbosity => Verbosity.Images;
+}
