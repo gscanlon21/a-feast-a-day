aa79331a9f58fef99a57cd7e3cb188306fde97d1
Graham Scanlon <git@ascallion.com>
2025-12-04 20:40:40 -0600

    feat: partial colors

--- Core/Code/Extensions/StringExtensions.cs
+++ Core/Code/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Runtime.CompilerServices;
+using System.Text;
+
 namespace Core.Code.Extensions;
 
 public static class StringExtensions
@@ -16,5 +19,34 @@ public static class StringExtensions
     public static bool IsWhiteSpace(this string? str)
     {
         return !string.IsNullOrEmpty(str) && string.IsNullOrWhiteSpace(str);
+    }
+
+    /// <summary>
+    /// Model.BooLean.ToCssClass() -> "boo-lean";
+    /// </summary>
+    public static string ToCssClass(this bool boolean, [CallerArgumentExpression(nameof(boolean))] string valueExp = "")
+    {
+        return boolean ? valueExp.Split('.')[^1].ToHtmlSlug() : string.Empty;
+    }
+
+    public static string ToHtmlSlug(this string str)
+    {
+        var stringBuilder = new StringBuilder();
+        char previousChar = char.MinValue;
+        foreach (char c in str)
+        {
+            if (char.IsUpper(c) || c == ' ' || c == '_')
+            {
+                if (stringBuilder.Length != 0 && previousChar != '-')
+                {
+                    stringBuilder.Append('-');
+                }
+            }
+
+            stringBuilder.Append(c);
+            previousChar = c;
+        }
+
+        return stringBuilder.ToString().ToLowerInvariant();
     }
 }
