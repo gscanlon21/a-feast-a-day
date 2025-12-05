dc25c56b682f830570e72e3db9d4699a0a81643d
Graham Scanlon <git@ascallion.com>
2025-12-04 21:18:55 -0600

    feat: partial colors

--- Core/Code/Extensions/StringExtensions.cs
+++ Core/Code/Extensions/StringExtensions.cs
@@ -24,9 +24,19 @@ public static class StringExtensions
     /// <summary>
     /// Model.BooLean.ToCssClass() -> "boo-lean";
     /// </summary>
-    public static string ToCssClass(this bool boolean, [CallerArgumentExpression(nameof(boolean))] string valueExp = "")
+    public static string? ToClass(this bool boolean, string? trueString = null, string? falseString = null, [CallerArgumentExpression(nameof(boolean))] string valueExp = "")
     {
-        return boolean ? valueExp.Split('.')[^1].ToHtmlSlug() : string.Empty;
+        return boolean ? (trueString ?? valueExp.Split('.')[^1].ToHtmlSlug()) : falseString;
+    }
+
+    public static string? ToStyle(this bool boolean, string trueString, string? falseString = null)
+    {
+        return boolean ? trueString?.EndWith(';') : falseString?.EndWith(';');
+    }
+
+    public static string? EndWith(this string? str, char append)
+    {
+        return str == null ? str : str.TrimEnd(append) + append;
     }
 
     public static string ToHtmlSlug(this string str)
