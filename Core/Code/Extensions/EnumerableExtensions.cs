b4fb19629992ffa8f46e3a6bfab893d521d393f0
Graham Scanlon <git@ascallion.com>
2026-01-08 20:32:39 -0600

    refactor: remove constraints

--- Core/Code/Extensions/EnumerableExtensions.cs
+++ Core/Code/Extensions/EnumerableExtensions.cs
@@ -108,7 +108,6 @@ public static class EnumerableExtensions
     }
 
     public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : struct
     {
         return nullOrder switch
         {
@@ -119,7 +118,6 @@ public static class EnumerableExtensions
     }
 
     public static IOrderedEnumerable<T> ThenBy<T, TKey>(this IOrderedEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : struct
     {
         return nullOrder switch
         {
@@ -130,7 +128,6 @@ public static class EnumerableExtensions
     }
 
     public static IOrderedEnumerable<T> OrderByDescending<T, TKey>(this IEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : struct
     {
         return nullOrder switch
         {
@@ -141,7 +138,6 @@ public static class EnumerableExtensions
     }
 
     public static IOrderedEnumerable<T> ThenByDescending<T, TKey>(this IOrderedEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : struct
     {
         return nullOrder switch
         {
@@ -151,61 +147,17 @@ public static class EnumerableExtensions
         };
     }
 
-    public static IOrderedEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : class
+    internal class NullableComparer<T> : IComparer<T?> 
     {
-        return nullOrder switch
-        {
-            NullOrder.NullsLast => list.OrderBy(keySelector, ClassComparer<TKey>.Larger),
-            NullOrder.NullsFirst => list.OrderBy(keySelector, ClassComparer<TKey>.Smaller),
-            _ => throw new ArgumentOutOfRangeException(nameof(nullOrder), nullOrder, null),
-        };
-    }
-
-    public static IOrderedEnumerable<T> ThenBy<T, TKey>(this IOrderedEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : class
-    {
-        return nullOrder switch
-        {
-            NullOrder.NullsLast => list.ThenBy(keySelector, ClassComparer<TKey>.Larger),
-            NullOrder.NullsFirst => list.ThenBy(keySelector, ClassComparer<TKey>.Smaller),
-            _ => throw new ArgumentOutOfRangeException(nameof(nullOrder), nullOrder, null),
-        };
-    }
-
-    public static IOrderedEnumerable<T> OrderByDescending<T, TKey>(this IEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : class
-    {
-        return nullOrder switch
-        {
-            NullOrder.NullsLast => list.OrderByDescending(keySelector, ClassComparer<TKey>.Smaller),
-            NullOrder.NullsFirst => list.OrderByDescending(keySelector, ClassComparer<TKey>.Larger),
-            _ => throw new ArgumentOutOfRangeException(nameof(nullOrder), nullOrder, null),
-        };
-    }
-
-    public static IOrderedEnumerable<T> ThenByDescending<T, TKey>(this IOrderedEnumerable<T> list, Func<T, TKey?> keySelector, NullOrder nullOrder)
-        where TKey : class
-    {
-        return nullOrder switch
-        {
-            NullOrder.NullsLast => list.ThenByDescending(keySelector, ClassComparer<TKey>.Smaller),
-            NullOrder.NullsFirst => list.ThenByDescending(keySelector, ClassComparer<TKey>.Larger),
-            _ => throw new ArgumentOutOfRangeException(nameof(nullOrder), nullOrder, null),
-        };
-    }
-
-    internal class ClassComparer<T> : IComparer<T?> where T : class
-    {
-        public static ClassComparer<T> Larger => new ClassComparer<T>(true);
-        public static ClassComparer<T> Smaller => new ClassComparer<T>(false);
-
         private readonly bool _isLarger;
 
-        private ClassComparer(bool isLarger)
+        private NullableComparer(bool isLarger)
         {
             _isLarger = isLarger;
         }
+
+        public static NullableComparer<T> Larger => new(true);
+        public static NullableComparer<T> Smaller => new(false);
 
         public int Compare(T? x, T? y)
         {
@@ -225,39 +177,6 @@ public static class EnumerableExtensions
             }
 
             return Comparer<T>.Default.Compare(x, y);
-        }
-    }
-
-    internal class NullableComparer<T> : IComparer<T?> where T : struct
-    {
-        public static NullableComparer<T> Larger => new NullableComparer<T>(true);
-        public static NullableComparer<T> Smaller => new NullableComparer<T>(false);
-
-        private readonly bool _isLarger;
-
-        private NullableComparer(bool isLarger)
-        {
-            _isLarger = isLarger;
-        }
-
-        public int Compare(T? x, T? y)
-        {
-            if (x == null && y == null)
-            {
-                return 0;
-            }
-
-            if (x == null)
-            {
-                return _isLarger ? 1 : -1;
-            }
-
-            if (y == null)
-            {
-                return _isLarger ? -1 : 1;
-            }
-
-            return Comparer<T>.Default.Compare(x.Value, y.Value);
         }
     }
 
