b79eac97a2a803b6d3af5d9d9281a49d3bb96500
Graham Scanlon <git@ascallion.com>
2026-03-04 20:30:26 -0600

    fix: upper bounds

--- Data/Code/Extensions/UserFamilyExtensions.cs
+++ Data/Code/Extensions/UserFamilyExtensions.cs
@@ -7,13 +7,15 @@ namespace Data.Code.Extensions;
 
 public static class UserFamilyExtensions
 {
+    /// <summary>
+    /// Returns the RDA as 100% and the TUL as a percentage of the RDA.
+    /// If there is no TUL, uses <see cref="UserConsts.NutrientTargetTULDefault"/>%.
+    /// </summary>
     public static Range DefaultRange(this ICollection<UserFamily> userFamilies, Nutrients nutrient)
     {
         var sumRDA = userFamilies.Where(f => nutrient.DailyAllowance(f.Person) != null).Average(f => nutrient.DailyAllowance(f.Person)!.RDA);
         var sumTUL = userFamilies.Where(f => nutrient.DailyAllowance(f.Person) != null).Average(f => nutrient.DailyAllowance(f.Person)!.TUL) ?? sumRDA * 2;
-        var tulDefault = Math.Min(UserConsts.NutrientTargetTULDefault, (sumTUL.HasValue && sumRDA.HasValue)
-            ? (int)Math.Ceiling(sumTUL.Value / sumRDA.Value * 100)
-            : UserConsts.NutrientTargetTULDefault);
+        var tulDefault = (sumTUL.HasValue && sumRDA.HasValue) ? (int)Math.Ceiling(sumTUL.Value / sumRDA.Value * 100) : UserConsts.NutrientTargetTULDefault;
 
         return new Range(UserConsts.NutrientTargetDefaultPercent, tulDefault);
     }
