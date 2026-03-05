1f4b78bc0b455df334456d68f5f585fa4272428c
Graham Scanlon <git@ascallion.com>
2026-03-04 19:56:44 -0600

    chore: update styles

--- Core/Models/Nutrients/Person.cs
+++ Core/Models/Nutrients/Person.cs
@@ -103,3 +103,16 @@ public enum Person
         | Male_51_70_Years | Female_51_70_Years
         | Male_71_XX_Years | Female_71_XX_Years
 }
+
+public static class PersonHelpers
+{
+    /// <summary>
+    /// Since signups are only for people 18+.
+    /// </summary>
+    public static readonly Person[] SignupPeople = 
+    [
+        Person.Male_19_30_Years, Person.Female_19_30_Years, Person.Pregnant_19_30_Years, Person.Lactating_19_30_Years,
+        Person.Male_31_50_Years, Person.Female_31_50_Years, Person.Pregnant_31_50_Years, Person.Lactating_31_50_Years,
+        Person.Male_51_70_Years, Person.Female_51_70_Years, Person.Male_71_XX_Years, Person.Female_71_XX_Years
+    ];
+}
