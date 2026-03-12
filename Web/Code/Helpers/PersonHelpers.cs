46f07a56b285812910beabd6abfd06c9d718de51
Graham Scanlon <git@ascallion.com>
2026-03-11 21:26:01 -0500

    fix: 18+ signups

--- Web/Code/Helpers/PersonHelpers.cs
+++ Web/Code/Helpers/PersonHelpers.cs
@@ -8,6 +8,7 @@ public static class PersonHelpers
     /// </summary>
     public static readonly Person[] SignupPeople =
     [
+        Person.Male_14_18_Years, Person.Female_14_18_Years, Person.Pregnant_14_18_Years, Person.Lactating_14_18_Years,
         Person.Male_19_30_Years, Person.Female_19_30_Years, Person.Pregnant_19_30_Years, Person.Lactating_19_30_Years,
         Person.Male_31_50_Years, Person.Female_31_50_Years, Person.Pregnant_31_50_Years, Person.Lactating_31_50_Years,
         Person.Male_51_70_Years, Person.Female_51_70_Years, Person.Male_71_XX_Years, Person.Female_71_XX_Years
